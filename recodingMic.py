0320b50b2cf4c5fa9187b24845c9bcf255cc39d5
PARKYOONJUNG <dbsdl315@gmail.com>
2019-08-11 17:52:38 +0900

    update ~~~
    update ~~~

--- recodingMic.py
+++ recodingMic.py
@@ -1,13 +1,23 @@
 # NOTE: this example requires PyAudio because it uses the Microphone class
-
 import speech_recognition as sr
 
 # obtain audio from the microphone
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
 
-# write audio to a WAV file
-with open("microphone-results.wav", "wb") as f:
-    f.write(audio.get_wav_data())
+
+file_name = str(input("What is your Name?"))
+count = 1
+
+for i in range(10) : # 10번 반
+    with sr.Microphone() as source:
+        print("Say Bed-Bird-Cat-Dog-Down !")
+        audio = r.listen(source)
+
+    result_name = file_name + "_" + str(count)
+    print(result_name)
+    # write audio to a WAV file
+    with open(result_name+".wav", "wb") as f:
+        f.write(audio.get_wav_data())
+        print(result_name + " : Finish !")
+
+    count += 1
